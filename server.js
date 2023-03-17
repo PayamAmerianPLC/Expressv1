859f6a3ad1509c1798441eb8944049c2c7bf1e7c
Payam <cpcpayam@gmail.com>
2023-03-17 15:00:25 +1000

    ejs

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express")//import AOI
 const app=express()// Drag Drop AOI/Argument aoi tagname is app
-app.set("view engine","ejs")
+app.set('views', './views');
+app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
     console.log("Here")    
-    res.render("views/index")//Default folder is views
+    res.render("index")//Default folder is views
 })
 
 app.listen(3000)
