fd4641f2b291ac09cbdcd336e76c9a4fbd5bfb17
Payam Amerian <payamamerian@gmail.com>
2023-03-25 20:15:09 +1000

    update

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");//import AOI
+const {Configuration, OpenAIApi } = require("openai");
+
 require("dotenv").config;
+const app=express()// Drag Drop AOI/Argument aoi tagname is app
 
 
-const{Configuration, OpenAIApi } = require(openai);
+
 app.use(express.json());
+
 const configuration= new Configuration({
     apikey: process.env.OPEN_AI_KEY
 })
 const openai=new OpenAIApi(Configuration);
-
-const app=express()// Drag Drop AOI/Argument aoi tagname is app
 app.set('views', './views');
 app.set("view engine","ejs");
 app.get("/",(req,res)=>{
